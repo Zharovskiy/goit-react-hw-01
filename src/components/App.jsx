4bf7bf2852fb02f30b317993031c1a87c2528e38
Yurii <jarovski08@gmail.com>
2024-03-06 17:26:02 +0200

    home work 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,18 @@
-// src/App.jsx
-
-import BookList from './Product.jsx';
+import Profile from "./Profile.jsx";
+import userData from "../userData.json";
 
 const App = () => {
   return (
     <>
-			<h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
   );
 };
 
 export default App;
-
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" }
-];
